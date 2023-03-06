2e9cf95d3e885dedd6626e727166d6785ad80a40
karoljanowski <karol.jj@icloud.com>
2023-03-06 18:38:55 +0100

    fix components

--- src/pages/home-page/scripts/home-page.js
+++ src/pages/home-page/scripts/home-page.js
@@ -2,6 +2,10 @@
 /// Here we import all the JavaScript files we need for our homepage.
 
 import '../styles/home-page.scss'
+import '../styles/plan-box.scss'
+import '../styles/texts.scss'
+import '../styles/buttons.scss'
+
 
 import loading from '../../../global-scripts/scripts/loading'
 loading()
