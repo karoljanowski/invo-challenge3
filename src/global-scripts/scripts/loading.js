7395b4c13bb7a696dd858455df0b5f1405579ea8
karoljanowski <karol.jj@icloud.com>
2023-03-06 18:10:26 +0100

    fix design

--- src/global-scripts/scripts/loading.js
+++ src/global-scripts/scripts/loading.js
@@ -1,10 +1,10 @@
 export default function loading() {
-    const button = document.querySelector('button')
+    const button = document.querySelector('.order__card__button')
     button.addEventListener('click', function () {
-        let loader = document.querySelector('.load')
-        let text = document.querySelector('#text')
+        let loader = document.querySelector('.order__card__button__img')
+        let text = document.getElementById('text')
+        text.textContent = ''
         loader.style.display = 'block'
-        text.textContent = 'Loading'
 
 
     })
